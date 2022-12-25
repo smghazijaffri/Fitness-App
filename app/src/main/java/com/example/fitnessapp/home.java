4c54819f6d94eb282dc6d35e919b35c0202894ff
Ghazi <smghazi14@gmail.com>
2022-12-25 14:18:10 +0500

    Intents

--- app/src/main/java/com/example/fitnessapp/home.java
+++ app/src/main/java/com/example/fitnessapp/home.java
@@ -8,13 +8,16 @@ import android.os.Bundle;
 import android.view.View;
 
 public class home extends AppCompatActivity {
-    CardView FBody, LBody;
+    CardView FBody, LBody, ABegin, CBegin, ArmBegin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         FBody = findViewById(R.id.fullbody);
         LBody = findViewById(R.id.lowerbody);
+        ABegin = findViewById(R.id.absbegin);
+        CBegin = findViewById(R.id.chestbegin);
+        ArmBegin = findViewById(R.id.armbegin);
 
         FBody.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,6 +29,24 @@ public class home extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(home.this, lowerbody.class));
+            }
+        });
+        ABegin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(home.this, absbeginner.class));
+            }
+        });
+        CBegin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(home.this, chestbeginner.class));
+            }
+        });
+        ArmBegin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(home.this, armbeginner.class));
             }
         });
     }
