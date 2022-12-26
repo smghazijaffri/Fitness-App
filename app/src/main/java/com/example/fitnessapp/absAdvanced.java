964ed9532a88476bcb469bfd89ca2c578c6da8e1
rameezjafry <syedrameezjafry@outlook.com>
2022-12-27 00:34:55 +0500

    shoulderbeginner+++

---
+++ app/src/main/java/com/example/fitnessapp/absAdvanced.java
@@ -0,0 +1,14 @@
+package com.example.fitnessapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class absAdvanced extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_abs_advanced);
+    }
+}
---
+++ app/src/main/java/com/example/fitnessapp/chestAdvanced.java
@@ -0,0 +1,13 @@
+package com.example.fitnessapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class chestAdvanced extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chest_advanced);
+    }
+}
