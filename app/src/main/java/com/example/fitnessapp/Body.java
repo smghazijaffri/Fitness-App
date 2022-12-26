1ce129116e21b86bc183a286b210b2478916c6a8
rameezjafry <syedrameezjafry@outlook.com>
2022-12-26 21:05:56 +0500

    shoulderbeginner

--- app/src/main/java/com/example/fitnessapp/Body.java
+++ app/src/main/java/com/example/fitnessapp/Body.java
@@ -2,6 +2,7 @@ package com.example.fitnessapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
